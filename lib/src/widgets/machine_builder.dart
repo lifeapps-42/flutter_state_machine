3b3c3cb42edd1a2e3b65b3ad0aaed6b2a93e0f4f
Max <maxstatusauto@gmail.com>
2022-07-04 16:35:24 +0300

    listener

--- lib/src/widgets/machine_builder.dart
+++ lib/src/widgets/machine_builder.dart
@@ -4,9 +4,65 @@ import 'package:flutter/material.dart';
 
 import '../../state_machine.dart';
 
-enum BuildFilter {
-  equal,
-  type,
+class Target<S, T> {
+  final T? Function(S state) target;
+
+  const Target(this.target);
+
+  T? call(S state) => target(state);
+}
+
+abstract class StateFilter<S extends Object> {
+  const StateFilter();
+  bool Function(S oldState, S newState) get shouldIgnore;
+}
+
+class EqualStateFilter<S extends Object> extends StateFilter<S> {
+  const EqualStateFilter();
+  @override
+  bool Function(S oldState, S newState) get shouldIgnore {
+    return (oldState, newState) => oldState == newState;
+  }
+}
+
+class TypeStateFilter<S extends Object> extends StateFilter<S> {
+  const TypeStateFilter();
+  @override
+  bool Function(S oldState, S newState) get shouldIgnore {
+    return (oldState, newState) => oldState.runtimeType == newState.runtimeType;
+  }
+}
+
+class PreciseStateFilter<S extends Object, T> extends StateFilter<S> {
+  final Target<S, T> target;
+  const PreciseStateFilter(this.target);
+  @override
+  bool Function(S oldState, S newState) get shouldIgnore {
+    return (oldState, newState) => target(oldState) == target(newState);
+  }
+}
+
+class MultiplePreciseStateFilter<S extends Object> extends StateFilter<S> {
+  final Set<Target<S, Object>> targets;
+  const MultiplePreciseStateFilter(this.targets);
+
+  @override
+  bool Function(S oldState, S newState) get shouldIgnore =>
+      _didAnyTargetChanged;
+
+  bool _didAnyTargetChanged(S oldState, S newState) {
+    for (final target in targets) {
+      if (target(oldState) != target(newState)) return true;
+    }
+    return false;
+  }
+}
+
+class CustomStateFilter<S extends Object> extends StateFilter<S> {
+  final bool Function(S oldState, S newState) ignoreWhen;
+  const CustomStateFilter(this.ignoreWhen);
+  @override
+  bool Function(S oldState, S newState) get shouldIgnore => ignoreWhen;
 }
 
 class MachineBuilder<S extends Object> extends StatefulWidget {
@@ -14,31 +70,26 @@ class MachineBuilder<S extends Object> extends StatefulWidget {
     Key? key,
     required this.machineFactory,
     required this.builder,
-    this.buildFilter = BuildFilter.equal,
+    this.stateFilter = const EqualStateFilter(),
+    this.child = const SizedBox(),
   }) : super(key: key);
 
   final MachineFactory<S> machineFactory;
-  final Widget Function(BuildContext context, S state) builder;
-  final BuildFilter buildFilter;
+  final Widget Function(BuildContext context, S state, Widget child) builder;
+  final StateFilter stateFilter;
+  final Widget child;
 
   @override
-  State<MachineBuilder> createState() =>
-      _MachineBuilderState<StateMachine<S>, S>();
+  State<MachineBuilder> createState() => _MachineBuilderState<S>();
 }
 
-class _MachineBuilderState<M extends StateMachine<S>, S extends Object>
-    extends State<MachineBuilder<S>> {
+class _MachineBuilderState<S extends Object> extends State<MachineBuilder<S>> {
   late final MachineFactory<S> _machineFactory;
   late S _state;
   late final StreamSubscription<S> _stateSubscription;
 
   bool _shouldIgnore(S newState) {
-    switch (widget.buildFilter) {
-      case BuildFilter.equal:
-        return _state == newState;
-      case BuildFilter.type:
-        return _state.runtimeType == newState.runtimeType;
-    }
+    return widget.stateFilter.shouldIgnore(_state, newState);
   }
 
   void _maybeRebuild(S newState) {
@@ -70,6 +121,64 @@ class _MachineBuilderState<M extends StateMachine<S>, S extends Object>
 
   @override
   Widget build(BuildContext context) {
-    return widget.builder(context, _state);
+    return widget.builder(context, _state, widget.child);
+  }
+}
+
+class MachineListener<S extends Object> extends StatefulWidget {
+  const MachineListener({
+    Key? key,
+    required this.machineFactory,
+    required this.onChange,
+    this.stateFilter = const EqualStateFilter(),
+    this.child = const SizedBox(),
+  }) : super(key: key);
+
+  final MachineFactory<S> machineFactory;
+  final void Function(BuildContext context, S oldState, S newState) onChange;
+  final StateFilter stateFilter;
+  final Widget child;
+
+  @override
+  State<MachineListener<S>> createState() => _MachineListenerState<S>();
+}
+
+class _MachineListenerState<S extends Object>
+    extends State<MachineListener<S>> {
+  late final MachineFactory<S> _machineFactory;
+  late S _state;
+  late final StreamSubscription<S> _stateSubscription;
+
+  bool _shouldIgnore(S newState) {
+    return widget.stateFilter.shouldIgnore(_state, newState);
+  }
+
+  void _maybeCall(S newState) {
+    if (!mounted) return;
+    if (_shouldIgnore(newState)) return;
+    widget.onChange(context, _state, newState);
+  }
+
+  void _subscribeToMachine() {
+    _stateSubscription = widget.machineFactory.stateStream.listen(_maybeCall);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _machineFactory = widget.machineFactory;
+    _state = _machineFactory.state;
+    _subscribeToMachine();
+  }
+
+  @override
+  void dispose() {
+    _stateSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
   }
 }
--- lib/src/widgets/sub_machine_builder.dart
+++ lib/src/widgets/sub_machine_builder.dart
@@ -1,58 +1,58 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import '../../state_machine.dart';
+// import '../../state_machine.dart';
 
-class SubmachineBuilder<S extends Object, T> extends StatefulWidget {
-  const SubmachineBuilder({
-    Key? key,
-    required this.machineFactory,
-    required this.builder,
-    this.buildFilter = BuildFilter.equal,
-    required this.subStateFactory,
-  }) : super(key: key);
+// class SubmachineBuilder<S extends Object, T> extends StatefulWidget {
+//   const SubmachineBuilder({
+//     Key? key,
+//     required this.machineFactory,
+//     required this.builder,
+//     this.buildFilter = BuildFilter.equal,
+//     required this.subStateFactory,
+//   }) : super(key: key);
 
-  final MachineFactory<S> machineFactory;
-  final Widget Function(BuildContext context, S state, T subState) builder;
-  final T Function(S state) subStateFactory;
-  final BuildFilter buildFilter;
+//   final MachineFactory<S> machineFactory;
+//   final Widget Function(BuildContext context, S state, T subState) builder;
+//   final T Function(S state) subStateFactory;
+//   final BuildFilter buildFilter;
 
-  @override
-  State<SubmachineBuilder<S, T>> createState() =>
-      _SubmachineBuilderState<S, T>();
-}
+//   @override
+//   State<SubmachineBuilder<S, T>> createState() =>
+//       _SubmachineBuilderState<S, T>();
+// }
 
-class _SubmachineBuilderState<S extends Object, T>
-    extends State<SubmachineBuilder<S, T>> {
-  T? _currentSubState;
-  Widget? _currentChild;
+// class _SubmachineBuilderState<S extends Object, T>
+//     extends State<SubmachineBuilder<S, T>> {
+//   T? _currentSubState;
+//   Widget? _currentChild;
 
-  bool _shouldIgnore(T newSubState) {
-    final old = _currentSubState;
-    if (old == null || _currentChild == null) return false;
+//   bool _shouldIgnore(T newSubState) {
+//     final old = _currentSubState;
+//     if (old == null || _currentChild == null) return false;
 
-    switch (widget.buildFilter) {
-      case BuildFilter.equal:
-        return old == newSubState;
-      case BuildFilter.type:
-        return old.runtimeType == newSubState.runtimeType;
-    }
-  }
+//     switch (widget.buildFilter) {
+//       case BuildFilter.equal:
+//         return old == newSubState;
+//       case BuildFilter.type:
+//         return old.runtimeType == newSubState.runtimeType;
+//     }
+//   }
 
-  Widget _handleNewState(BuildContext context, S newState) {
-    final subState = widget.subStateFactory(newState);
-    if (_shouldIgnore(subState)) return _currentChild!;
-    final newChild = widget.builder(context, newState, subState);
-    _currentSubState = subState;
-    _currentChild = newChild;
-    return newChild;
-  }
+//   Widget _handleNewState(BuildContext context, S newState) {
+//     final subState = widget.subStateFactory(newState);
+//     if (_shouldIgnore(subState)) return _currentChild!;
+//     final newChild = widget.builder(context, newState, subState);
+//     _currentSubState = subState;
+//     _currentChild = newChild;
+//     return newChild;
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MachineBuilder(
-      machineFactory: widget.machineFactory,
-      buildFilter: BuildFilter.equal,
-      builder: _handleNewState,
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MachineBuilder(
+//       machineFactory: widget.machineFactory,
+//       buildFilter: BuildFilter.equal,
+//       builder: _handleNewState,
+//     );
+//   }
+// }
